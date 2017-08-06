5dd479fad0c088ff41a12dfea83c11f131b2dec4
Prit Raj <prit.adra@gmail.com>
2017-08-06 16:50:02 +0530

    Video Search done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends Component{
-    constructor(prop){
-        super(prop);
-        this.state={term: ''};
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
     }
 
-    render(){
-        return <div>
-            <input
-                value={this.state.term}
-                onChange={event=>this.setState({term:event.target.value})}/>
-            val: {this.state.term}
-        </div>;
+    render() {
+        return (
+            <div className='search-bar'>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)} />
+            </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
     }
 }
 
